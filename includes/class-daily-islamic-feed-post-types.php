d90e957af0a50ded4aa9a19b73fffaa6bbfdd13a
NurdinDev <nureddinbedevi@gmail.com>
2020-10-15 17:11:41 +0300

    Register custom post types for Hadith, Ayah, Names-of-allah

---
+++ includes/class-daily-islamic-feed-post-types.php
@@ -0,0 +1,143 @@
+<?php
+
+/**
+ * Register Custom Post Types
+ *
+ * Loads and defines the internationalization files for this plugin
+ * so that it is ready for translation.
+ *
+ * @link       http://example.com
+ * @since      1.0.0
+ *
+ * @package    Daily_Islamic_Feed
+ * @subpackage Daily_Islamic_Feed/includes
+ */
+
+/**
+ * Register Custom Post Types.
+ *
+ * Loads and defines the internationalization files for this plugin
+ * so that it is ready for translation.
+ *
+ * @since      1.0.0
+ * @package    Daily_Islamic_Feed
+ * @subpackage Daily_Islamic_Feed/includes
+ * @author     Your Name <email@example.com>
+ */
+class Daily_Islamic_Feed_Post_Types
+{
+
+	/**
+	 * Static custom posts names
+	 *
+	 * @var string
+	 */
+	public static $AYAH = 'ayah';
+	public static $HADITH = 'hadith';
+	public static $NAMES = 'names-of-allah';
+
+
+	/**
+	 * The ID of this plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 * @var      string    $daily_islamic_feed    The ID of this plugin.
+	 */
+	private $daily_islamic_feed;
+
+	/**
+	 * Initialize the class and set its properties.
+	 *
+	 * @since    1.0.0
+	 * @param      string    $daily_islamic_feed       The name of this plugin.
+	 * @param      string    $version    The version of this plugin.
+	 */
+	public function __construct($daily_islamic_feed)
+	{
+
+		$this->daily_islamic_feed = $daily_islamic_feed;
+	}
+
+	/**
+	 * Load the plugin text domain for translation.
+	 *
+	 * @since    1.0.0
+	 */
+
+	function register_post_types()
+	{
+
+		// Ayah post type.
+		register_post_type(self::$AYAH, array(
+			'labels'       => array(
+				'name'                  => __('Ayah', $this->daily_islamic_feed),
+				'singular_name'         => __('Ayah', $this->daily_islamic_feed),
+				'add_new'               => __('Add New Ayah', $this->daily_islamic_feed),
+				'add_new_item'          => __('Add New Ayah', $this->daily_islamic_feed),
+				'new_item'              => __('New Ayah', $this->daily_islamic_feed),
+				'edit_item'             => __('Edit Ayah', $this->daily_islamic_feed),
+				'view_item'             => __('View Ayah', $this->daily_islamic_feed),
+				'all_items'             => __('Ayat', $this->daily_islamic_feed),
+				'featured_image'        => __('Ayah Cover Image', $this->daily_islamic_feed),
+				'set_featured_image'    => __('Set Cover Image', $this->daily_islamic_feed),
+				'remove_featured_image' => __('Remove Cover Image', $this->daily_islamic_feed),
+				'use_featured_image'    => __('Use Cover Image', $this->daily_islamic_feed),
+			),
+			'public'       => true,
+			'show_in_rest' => true,
+			'menu_icon'    => 'dashicons-building',
+			'support'      => array('title', 'editor', 'thumbnail'),
+			'show_in_menu' => 'inspiration-menu-top-level',
+			'taxonomies'   => array('category', 'post_tag', 'schedule')
+		));
+
+		// Hadit Post Type.
+		register_post_type(self::$HADITH, array(
+			'labels'       => array(
+				'name'                  => __('Hadith', $this->daily_islamic_feed),
+				'singular_name'         => __('Hadith', $this->daily_islamic_feed),
+				'add_new'               => __('Add New Hadith', $this->daily_islamic_feed),
+				'add_new_item'          => __('Add New Hadith', $this->daily_islamic_feed),
+				'new_item'              => __('New Hadith', $this->daily_islamic_feed),
+				'edit_item'             => __('Edit Hadith', $this->daily_islamic_feed),
+				'view_item'             => __('View Hadith', $this->daily_islamic_feed),
+				'all_items'             => __('Hadith', $this->daily_islamic_feed),
+				'featured_image'        => __('Hadith Cover Image', $this->daily_islamic_feed),
+				'set_featured_image'    => __('Set Cover Image', $this->daily_islamic_feed),
+				'remove_featured_image' => __('Remove Cover Image', $this->daily_islamic_feed),
+				'use_featured_image'    => __('Use Cover Image', $this->daily_islamic_feed)
+			),
+			'public'       => true,
+			'show_in_rest' => true,
+			'menu_icon'    => 'dashicons-building',
+			'support'      => array('title', 'editor', 'thumbnail'),
+			'show_in_menu' => 'inspiration-menu-top-level',
+			'taxonomies'   => array('category', 'post_tag', 'schedule'),
+		));
+
+
+		// Names Of Allah Post Types.
+		register_post_type(self::$NAMES, array(
+			'labels'       => array(
+				'name'                  => __('Names of Allah', $this->daily_islamic_feed),
+				'singular_name'         => __('Name of Allah', $this->daily_islamic_feed),
+				'add_new'               => __('Add New Name', $this->daily_islamic_feed),
+				'add_new_item'          => __('Add New Name', $this->daily_islamic_feed),
+				'new_item'              => __('New Name', $this->daily_islamic_feed),
+				'edit_item'             => __('Edit Name', $this->daily_islamic_feed),
+				'view_item'             => __('View Name', $this->daily_islamic_feed),
+				'all_items'             => __('Names of Allah', $this->daily_islamic_feed),
+				'featured_image'        => __('Name Cover Image', $this->daily_islamic_feed),
+				'set_featured_image'    => __('Set Cover Image', $this->daily_islamic_feed),
+				'remove_featured_image' => __('Remove Cover Image', $this->daily_islamic_feed),
+				'use_featured_image'    => __('Use Cover Image', $this->daily_islamic_feed),
+			),
+			'public'       => true,
+			'show_in_rest' => true,
+			'menu_icon'    => 'dashicons-building',
+			'support'      => array('title', 'editor', 'thumbnail'),
+			'show_in_menu' => 'inspiration-menu-top-level'
+		));
+	}
+}
--- includes/class-daily-islamic-feed.php
+++ includes/class-daily-islamic-feed.php
@@ -30,199 +30,218 @@
 class Daily_Islamic_Feed
 {
 
-    /**
-     * The loader that's responsible for maintaining and registering all hooks that power
-     * the plugin.
-     *
-     * @since    1.0.0
-     * @access   protected
-     * @var      Daily_Islamic_Feed_Loader $loader Maintains and registers all hooks for the plugin.
-     */
-    protected $loader;
+	/**
+	 * The loader that's responsible for maintaining and registering all hooks that power
+	 * the plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   protected
+	 * @var      Daily_Islamic_Feed_Loader    $loader    Maintains and registers all hooks for the plugin.
+	 */
+	protected $loader;
 
-    /**
-     * The unique identifier of this plugin.
-     *
-     * @since    1.0.0
-     * @access   protected
-     * @var      string $daily_islamic_feed The string used to uniquely identify this plugin.
-     */
-    protected $daily_islamic_feed;
+	/**
+	 * The unique identifier of this plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   protected
+	 * @var      string    $daily_islamic_feed    The string used to uniquely identify this plugin.
+	 */
+	protected $daily_islamic_feed;
 
-    /**
-     * The current version of the plugin.
-     *
-     * @since    1.0.0
-     * @access   protected
-     * @var      string $version The current version of the plugin.
-     */
-    protected $version;
+	/**
+	 * The current version of the plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   protected
+	 * @var      string    $version    The current version of the plugin.
+	 */
+	protected $version;
 
-    /**
-     * Define the core functionality of the plugin.
-     *
-     * Set the plugin name and the plugin version that can be used throughout the plugin.
-     * Load the dependencies, define the locale, and set the hooks for the admin area and
-     * the public-facing side of the site.
-     *
-     * @since    1.0.0
-     */
-    public function __construct()
-    {
-        if (defined('DAILY_ISLAMIC_FEED_VERSION')) {
-            $this->version = DAILY_ISLAMIC_FEED_VERSION;
-        } else {
-            $this->version = '1.0.0';
-        }
+	/**
+	 * Define the core functionality of the plugin.
+	 *
+	 * Set the plugin name and the plugin version that can be used throughout the plugin.
+	 * Load the dependencies, define the locale, and set the hooks for the admin area and
+	 * the public-facing side of the site.
+	 *
+	 * @since    1.0.0
+	 */
+	public function __construct()
+	{
+		if (defined('DAILY_ISLAMIC_FEED_VERSION')) {
+			$this->version = DAILY_ISLAMIC_FEED_VERSION;
+		} else {
+			$this->version = '1.0.0';
+		}
 
-        if (defined('DAILY_ISLAMIC_FEED_DOMAIN')) {
-            $this->daily_islamic_feed = DAILY_ISLAMIC_FEED_DOMAIN;
-        } else {
-            $this->daily_islamic_feed = 'daily-islamic-feed';
-        }
+		if (defined('DAILY_ISLAMIC_FEED_DOMAIN')) {
+			$this->daily_islamic_feed = DAILY_ISLAMIC_FEED_DOMAIN;
+		} else {
+			$this->daily_islamic_feed = 'daily-islamic-feed';
+		}
 
-        $this->load_dependencies();
-        $this->set_locale();
-        $this->define_admin_hooks();
-        $this->define_public_hooks();
-    }
+		$this->load_dependencies();
+		$this->set_locale();
+		$this->define_admin_hooks();
+		$this->define_public_hooks();
+		$this->register_post_types();
+	}
 
-    /**
-     * Load the required dependencies for this plugin.
-     *
-     * Include the following files that make up the plugin:
-     *
-     * - Daily_Islamic_Feed_Loader. Orchestrates the hooks of the plugin.
-     * - Daily_Islamic_Feed_i18n. Defines internationalization functionality.
-     * - Daily_Islamic_Feed_Admin. Defines all hooks for the admin area.
-     * - Daily_Islamic_Feed_Public. Defines all hooks for the public side of the site.
-     *
-     * Create an instance of the loader which will be used to register the hooks
-     * with WordPress.
-     *
-     * @since    1.0.0
-     * @access   private
-     */
-    private function load_dependencies()
-    {
+	/**
+	 * Load the required dependencies for this plugin.
+	 *
+	 * Include the following files that make up the plugin:
+	 *
+	 * - Daily_Islamic_Feed_Loader. Orchestrates the hooks of the plugin.
+	 * - Daily_Islamic_Feed_i18n. Defines internationalization functionality.
+	 * - Daily_Islamic_Feed_Admin. Defines all hooks for the admin area.
+	 * - Daily_Islamic_Feed_Public. Defines all hooks for the public side of the site.
+	 *
+	 * Create an instance of the loader which will be used to register the hooks
+	 * with WordPress.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 */
+	private function load_dependencies()
+	{
 
-        /**
-         * The class responsible for orchestrating the actions and filters of the
-         * core plugin.
-         */
-        require_once plugin_dir_path(dirname(__FILE__)) . 'includes/class-daily-islamic-feed-loader.php';
+		/**
+		 * The class responsible for orchestrating the actions and filters of the
+		 * core plugin.
+		 */
+		require_once plugin_dir_path(dirname(__FILE__)) . 'includes/class-daily-islamic-feed-loader.php';
 
-        /**
-         * The class responsible for defining internationalization functionality
-         * of the plugin.
-         */
-        require_once plugin_dir_path(dirname(__FILE__)) . 'includes/class-daily-islamic-feed-i18n.php';
+		/**
+		 * The class responsible for defining internationalization functionality
+		 * of the plugin.
+		 */
+		require_once plugin_dir_path(dirname(__FILE__)) . 'includes/class-daily-islamic-feed-i18n.php';
 
-        /**
-         * The class responsible for defining all actions that occur in the admin area.
-         */
-        require_once plugin_dir_path(dirname(__FILE__)) . 'admin/class-daily-islamic-feed-admin.php';
+		/**
+		 * The class responsible for defining all actions that occur in the admin area.
+		 */
+		require_once plugin_dir_path(dirname(__FILE__)) . 'admin/class-daily-islamic-feed-admin.php';
 
-        /**
-         * The class responsible for defining all actions that occur in the public-facing
-         * side of the site.
-         */
-        require_once plugin_dir_path(dirname(__FILE__)) . 'public/class-daily-islamic-feed-public.php';
+		/**
+		 * The class responsible for defining all actions that occur in the public-facing
+		 * side of the site.
+		 */
+		require_once plugin_dir_path(dirname(__FILE__)) . 'public/class-daily-islamic-feed-public.php';
 
-        $this->loader = new Daily_Islamic_Feed_Loader();
+		$this->loader = new Daily_Islamic_Feed_Loader();
 
-    }
 
-    /**
-     * Define the locale for this plugin for internationalization.
-     *
-     * Uses the Daily_Islamic_Feed_i18n class in order to set the domain and to register the hook
-     * with WordPress.
-     *
-     * @since    1.0.0
-     * @access   private
-     */
-    private function set_locale()
-    {
+		require_once plugin_dir_path(dirname(__FILE__)) . 'includes/class-daily-islamic-feed-post-types.php';
+	}
 
-        $plugin_i18n = new Daily_Islamic_Feed_i18n();
+	/**
+	 * Define the locale for this plugin for internationalization.
+	 *
+	 * Uses the Daily_Islamic_Feed_i18n class in order to set the domain and to register the hook
+	 * with WordPress.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 */
+	private function set_locale()
+	{
 
-        $this->loader->add_action('plugins_loaded', $plugin_i18n, 'load_plugin_textdomain');
-    }
+		$plugin_i18n = new Daily_Islamic_Feed_i18n();
 
-    /**
-     * Register all of the hooks related to the admin area functionality
-     * of the plugin.
-     *
-     * @since    1.0.0
-     * @access   private
-     */
-    private function define_admin_hooks()
-    {
+		$this->loader->add_action('plugins_loaded', $plugin_i18n, 'load_plugin_textdomain');
+	}
+	/**
+	 * Define the locale for this plugin for internationalization.
+	 *
+	 * Uses the Daily_Islamic_Feed_i18n class in order to set the domain and to register the hook
+	 * with WordPress.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 */
+	private function register_post_types()
+	{
 
-        $plugin_admin = new Daily_Islamic_Feed_Admin($this->get_daily_islamic_feed(), $this->get_version());
+		$plugin_posts = new Daily_Islamic_Feed_Post_Types($this->get_daily_islamic_feed());
 
-        $this->loader->add_action('admin_enqueue_scripts', $plugin_admin, 'enqueue_styles');
-        $this->loader->add_action('admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts');
-    }
+		$this->loader->add_action('init', $plugin_posts, 'register_post_types');
+	}
 
-    /**
-     * Register all of the hooks related to the public-facing functionality
-     * of the plugin.
-     *
-     * @since    1.0.0
-     * @access   private
-     */
-    private function define_public_hooks()
-    {
+	/**
+	 * Register all of the hooks related to the admin area functionality
+	 * of the plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 */
+	private function define_admin_hooks()
+	{
 
-        $plugin_public = new Daily_Islamic_Feed_Public($this->get_daily_islamic_feed(), $this->get_version());
+		$plugin_admin = new Daily_Islamic_Feed_Admin($this->get_daily_islamic_feed(), $this->get_version());
 
-        $this->loader->add_action('wp_enqueue_scripts', $plugin_public, 'enqueue_styles');
-        $this->loader->add_action('wp_enqueue_scripts', $plugin_public, 'enqueue_scripts');
-    }
+		$this->loader->add_action('admin_enqueue_scripts', $plugin_admin, 'enqueue_styles');
+		$this->loader->add_action('admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts');
+	}
 
-    /**
-     * Run the loader to execute all of the hooks with WordPress.
-     *
-     * @since    1.0.0
-     */
-    public function run()
-    {
-        $this->loader->run();
-    }
+	/**
+	 * Register all of the hooks related to the public-facing functionality
+	 * of the plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 */
+	private function define_public_hooks()
+	{
 
-    /**
-     * The name of the plugin used to uniquely identify it within the context of
-     * WordPress and to define internationalization functionality.
-     *
-     * @return    string    The name of the plugin.
-     * @since     1.0.0
-     */
-    public function get_daily_islamic_feed()
-    {
-        return $this->daily_islamic_feed;
-    }
+		$plugin_public = new Daily_Islamic_Feed_Public($this->get_daily_islamic_feed(), $this->get_version());
 
-    /**
-     * The reference to the class that orchestrates the hooks with the plugin.
-     *
-     * @return    Daily_Islamic_Feed_Loader    Orchestrates the hooks of the plugin.
-     * @since     1.0.0
-     */
-    public function get_loader()
-    {
-        return $this->loader;
-    }
+		$this->loader->add_action('wp_enqueue_scripts', $plugin_public, 'enqueue_styles');
+		$this->loader->add_action('wp_enqueue_scripts', $plugin_public, 'enqueue_scripts');
+	}
 
-    /**
-     * Retrieve the version number of the plugin.
-     *
-     * @return    string    The version number of the plugin.
-     * @since     1.0.0
-     */
-    public function get_version()
-    {
-        return $this->version;
-    }
+	/**
+	 * Run the loader to execute all of the hooks with WordPress.
+	 *
+	 * @since    1.0.0
+	 */
+	public function run()
+	{
+		$this->loader->run();
+	}
+
+	/**
+	 * The name of the plugin used to uniquely identify it within the context of
+	 * WordPress and to define internationalization functionality.
+	 *
+	 * @since     1.0.0
+	 * @return    string    The name of the plugin.
+	 */
+	public function get_daily_islamic_feed()
+	{
+		return $this->daily_islamic_feed;
+	}
+
+	/**
+	 * The reference to the class that orchestrates the hooks with the plugin.
+	 *
+	 * @since     1.0.0
+	 * @return    Daily_Islamic_Feed_Loader    Orchestrates the hooks of the plugin.
+	 */
+	public function get_loader()
+	{
+		return $this->loader;
+	}
+
+	/**
+	 * Retrieve the version number of the plugin.
+	 *
+	 * @since     1.0.0
+	 * @return    string    The version number of the plugin.
+	 */
+	public function get_version()
+	{
+		return $this->version;
+	}
 }
